4d6d08c5bec30372cf56d6f5759c41af7c3e3315
root <root@WALL-T.localdomain>
2018-12-29 02:04:20 -0500

    Cleanup

--- basicVolumeInfo.py
+++ basicVolumeInfo.py
@@ -177,7 +177,7 @@ def getInfo(uuid: List[str]) -> List[List[Dict[str, Dict[str, int]]]]:
             path = infoPath(id, snap)
             if os.path.isfile(path):
                 with ConvertJSON(path) as info:
-                    if 'type' in info:
+                    if 'type' in info and info['type'].lower() == 'linux':
                         # Linux (info[type] => 'linux')
                         snaps.append(linux(info))
                     elif info['os'].lower().startswith('windows'):
@@ -524,7 +524,7 @@ class PresentNiceColumns:
         Format volume used/capacity values to the correct binary or metric
         magnitude (and hence prefix).
         """
-        print(bts, type(bts))
+        
         if bts < 0:
             raise ValueError('Expected value >=0, received {}'.format(bts))
 
