d1b9ac3b1cbef23ca823ac6706bc9ba37af30e77
bjd2385 <bjd2385@aperiodicity.com>
2019-02-17 22:41:10 -0500

    Updates

--- getThickVim.sh
+++ getThickVim.sh
@@ -2,10 +2,15 @@
 # Get a slimmed-down version of my .vimrc for use on a Datto. Limited since we
 # don't have git, and it's probably not a good idea to install it.
 
+apt update
+apt install git -y
+
 mkdir -p ~/.vim/bundle
-mkdir -p ~/.vim/colors
+mkdir ~/.vim/colors
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 wget -O "$HOME/.vimrc" "https://raw.githubusercontent.com/bjd2385/Linux-Configuration-Files/master/datto/.thickVimrc"
 wget -O "$HOME/.vim/colors/badwolf.vim" "https://raw.githubusercontent.com/sjl/badwolf/master/colors/badwolf.vim"
+
+vim +PluginInstall +qall
