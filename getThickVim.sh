3638a9966659e6067bc00f42b3224dc29500cee3
bjd2385 <bjd2385@aperiodicity.com>
2019-02-17 22:44:21 -0500

    Update bash script

--- getThickVim.sh
+++ getThickVim.sh
@@ -10,7 +10,7 @@ mkdir ~/.vim/colors
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-wget -O "$HOME/.vimrc" "https://raw.githubusercontent.com/bjd2385/Linux-Configuration-Files/master/datto/.thickVimrc"
+wget -O "$HOME/.vimrc" "https://raw.githubusercontent.com/bjd2385/datto/master/getThickVim.sh?token=APG-tDMgjlSSKdODirHfeHB6BCLsTFiYks5cain_wA%3D%3D"
 wget -O "$HOME/.vim/colors/badwolf.vim" "https://raw.githubusercontent.com/sjl/badwolf/master/colors/badwolf.vim"
 
 vim +PluginInstall +qall
