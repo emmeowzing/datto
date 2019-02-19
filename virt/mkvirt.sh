75b1ae37b6a2aa9e5dbc987653fa32d88305ca55
bjd2385 <bjd2385@aperiodicity.com>
2019-02-18 21:39:45 -0500

    Multiple cursors

---
+++ virt/mkvirt.sh
@@ -0,0 +1,165 @@
+#! /bin/bash
+# Make the process of deploying Windows virtualizations easier with 
+# libvirt/qemu-kvm. This isn't a perfect script, but as long as you enter the
+# proper information, it'll make your life a lot easier than sifting through
+# the manpage.
+#
+# A few notes about the default configuration offered here:
+#
+# --cpu host -- offers the best performance, but may cause problems if you ever
+#               migrate a VM from the environment it was created on. Modify
+#               this to `host-model-only` for a tradeoff between portability
+#               and performance.
+
+
+##
+# If all required packages aren't installed, install them.
+checkInstalled()
+{
+    if ! dpkg -s virtinst qemu-kvm libvirt-daemon-system libvirt-clients \
+        bridge-utils virt-viewer &>/dev/null
+    then
+        if [[ "$(getIO "install [y|n]")" =~ ^[^yY] ]]
+        then
+            printf "** ERROR: prerequisites not satisfied\\n" 1>&2
+            exit 1
+        fi
+
+        apt update -y
+        apt install virtinst qemu-kvm libvirt-daemon-system libvirt-clients \
+            bridge-utils virt-viewer -y
+    fi
+
+    printf "All prerequisites satisfied.\\n"
+}
+
+
+##
+# Get valid user input for various things.
+getIO()
+{
+    if [ $# != 1 ]
+    then
+        printf "** ERROR: \`getIO\` expected 1 argument, received %s\\n" \
+            "$#" 1>&2
+        exit 1
+    fi
+
+    local input 
+    
+    while true
+    do
+        read -r -p "Please enter $1: " input
+        if ! [ "$input" ]
+        then
+            printf "** ERROR: must enter non-empty string\\n" 1>&2
+        else
+            break
+        fi
+    done
+    
+    printf "%s" "$input"
+}
+
+
+##
+# Set up the VM with variables.
+setup()
+{
+    local name mem hostCPUs cpu disks ans installer
+
+    # Save 2 processors for the host, at the very least.
+    hostCPUs="$(nproc --ignore 2)"
+
+    name="$(getIO "the name")"
+    mem="$(getIO "the memory (in MiB)")"
+
+    # TODO: check against CPUs already taken up by *-active VMs.
+    while true
+    do
+        cpu="$(getIO "the number of CPUs")"
+        if [ "$cpu" -gt "$hostCPUs" ]
+        then
+            printf "**WARNING: number of available host CPUs is %s\\n" \
+                "$hostCPUs" 1>&2
+            if [[ "$(getIO "proceed? [y|n]")" =~ ^[yY] ]]
+            then
+                printf "**WARNING: proceeding with %s CPUs\\n" "$cpu" 1>&2
+                break
+            fi
+        fi
+        break
+    done
+
+    # Add disks.
+    disks=()
+    previous=false
+
+    read -r -p "Would you like to add disks now [y|n]: " ans
+    
+    if [[ "$ans" =~ ^[Yy] ]]
+    then
+        # Loop until we're done adding disks.
+        while true
+        do
+            disks+=("--disk")
+
+            # Set up the installer disk/ISO/img.
+            if ! [ $previous ]
+            then
+                read -r -p "Is this an installer? [y|n]" installer
+
+                if [ "$installer" ]
+                then
+                    # Get a valid installer.
+                    while true
+                    do
+                        read -r -p "Enter a valid file path: " path
+
+                        if ! [ -e "$path" ]
+                        then
+                            printf "**ERROR: enter a valid path, received %s\\n" \
+                                "$path"
+                        fi
+                    done
+
+                    disks[0]="${disks[0]} cdrom="
+                fi
+                previous=true
+            fi
+        done
+    fi
+
+    printf "virt-install --name %s --memory %s --arch %s --vcpus %s --cpu host
+        --security type=dynamic --clock=localtime " \
+        "$name" "$mem" "$(uname -m)" "$cpu"
+}
+
+
+main()
+{
+    local runnable input
+
+    # Ensure prerequisites are satisfied.
+    checkInstalled
+
+    # Set up the VM.
+    runnable="$(setup)"
+
+    printf "\033[1mString:\033[0m \033[32;1m%s\033[0m\\n" "$runnable"
+
+    read -r -p "Would you like to execute this string as a dry run [y|n]: " input
+    if [[ "$read" =~ ^[Yy] ]]
+    then
+        eval "$runnable \-\-dry-run"
+    fi
+
+    # Unset all the variables/function declarations.
+    unset -f checkInstalled
+    unset -f getIO
+    unset -f setup
+}
+
+
+main
+unset -f main
